516dadd49e880090680f90313a8f3fcdd000acdc
artasdaa <cellom293@gmail.com>
2025-10-01 08:31:51 +0700

    percobaan 1 change

--- IfCetakKRS12.java
+++ IfCetakKRS12.java
@@ -9,8 +9,7 @@ public class IfCetakKRS12 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         } else {
-            System.out.println("UKT belum terverifikasi");
-            System.out.println("Silakan lakukan pembayaran terlebih dahulu");
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
         sc.close();
     }
